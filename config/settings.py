e184f4b8dd6dbba8ba539c3b8185d72a0189fd5a
LevonAS <101938308+LevonAS@users.noreply.github.com>
2023-01-21 14:39:31 +0300

    Lesson 5 (#5)
    * lesson_5 Lesson 5 ready

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "markdownify.apps.MarkdownifyConfig",
+    "social_django",
     "mainapp",
+    "authapp",
 ]
 
 MIDDLEWARE = [
@@ -64,9 +66,12 @@ TEMPLATES = [
             "context_processors": [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
+                "django.template.context_processors.media",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "mainapp.context_processors.example.simple_context_processor",
+                "social_django.context_processors.backends",
+                "social_django.context_processors.login_redirect",
             ],
         },
     },
@@ -104,6 +109,21 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "authapp.CustomUser"
+
+AUTHENTICATION_BACKENDS = (
+    "social_core.backends.github.GithubOAuth2",
+    "social_core.backends.google.GoogleOAuth2",
+    "social_core.backends.yandex.YandexOAuth2",
+    "social_core.backends.steam.SteamOpenId",
+    "django.contrib.auth.backends.ModelBackend",
+)
+
+# LOGIN_URL = '/auth/login/google-oauth2/'
+LOGIN_REDIRECT_URL = "mainapp:main_page"
+LOGOUT_REDIRECT_URL = "mainapp:main_page"
+
+MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
@@ -132,3 +152,22 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+# Media files
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+
+SOCIAL_AUTH_GITHUB_KEY = "d9ff0510081096ab4656"
+SOCIAL_AUTH_GITHUB_SECRET = "9401c1baf17c965a8152b6e197d2d0f4ba7ee243"
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = "834073725128-7bprcq5kgc6ng64t5mukkigl9kssoqqh.apps.googleusercontent.com"
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = "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"
+
+SOCIAL_AUTH_YANDEX_OAUTH2_KEY = "369158b26a5d4a91979927bf229794d0"
+SOCIAL_AUTH_YANDEX_OAUTH2_SECRET = "984492ce736c43b48328f90b622e4a42"
+
+SOCIAL_AUTH_STEAM_API_KEY = "A709414B778D319627C5ED922AC112E9"
+SOCIAL_AUTH_STEAM_EXTRA_DATA = ["username"]
+
+# SOCIAL_AUTH_URL_NAMESPACE = 'social'
